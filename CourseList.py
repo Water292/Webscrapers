79f9a0134fb74d9474d72e4b7f6aa9cd2b1e420f
Aayush <aayush.bhasin@gmail.com>
2016-08-26 03:19:46 +0530

    get list of fall courses

---
+++ CourseList.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+
+fall = list()
+
+fallCourseUrl = "https://enr-apps.as.cmu.edu/assets/SOC/sched_layout_fall.htm"
+fallCoursePage = requests.get(fallCourseUrl)
+
+soup = BeautifulSoup(fallCoursePage.text, "html.parser")
+
+for row in soup.find_all("tr"):
+    cells = row.findAll('td')
+    course_id = cells[0].getText() 
+    if course_id.isdigit(): fall.append(course_id)
+
+
