14bbb74bab5c327f166ea7e1b1b531a9569ae98b
Aayush <aayush.bhasin@gmail.com>
2016-09-09 11:48:30 -0400

    added ece scraper

--- courseListGenerator.py
+++ courseListGenerator.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import copy
 
 fallCourseUrl = "https://enr-apps.as.cmu.edu/assets/SOC/sched_layout_fall.htm"
 springCourseUrl = "https://enr-apps.as.cmu.edu/assets/SOC/sched_layout_spring.htm"
@@ -25,6 +26,26 @@ class Webscrapers():
             courses.close()
 
     @staticmethod
+    def updateEceAreaCourses(tag, areaOrder, req):
+        requirements = {'DeviceSciences': list(),
+            'SignalsAndSystems': list(),
+            'Circuits': list(),
+            'HardwareSystems': list(),
+            'SoftwareSystems': list()}
+        if (req == 'coverage'): 
+            requirements['UndergraduateProjects'] = list()
+            requirements['ProfessionalPolicy'] = list()
+        areaTables = tag.findAll('table')
+        for i in range(len(areaTables)):
+            area = areaOrder[i]
+            for row in areaTables[i].findAll('tr'):
+                cells = row.findAll('td')
+                course_id = cells[0].getText().replace('-', '')
+                if course_id.isdigit(): 
+                    requirements[area].append(course_id)
+        return requirements
+
+    @staticmethod
     def eceCourseUpdate():
         # with open("courseList.json", "r") as courses:
         #     allCourses = json.load(courses)
@@ -36,8 +57,21 @@ class Webscrapers():
         soup = BeautifulSoup(page.text, "html.parser")
         for tag in soup.find_all('div', class_ = "accordion-inner"):
             if (tag['id'] == 'Area-Requirements'):
-                ece['Area-Requirements'] = {'DeviceSciences': list(),
-                    'SignalsAndSystems': list(),
-                    ''}
+                areaOrder = ['DeviceSciences', 'SignalsAndSystems', 'Circuits',
+                    'HardwareSystems', 'SoftwareSystems']
+                ece['Area-Requirements'] = (
+                    Webscrapers.updateEceAreaCourses(tag, areaOrder, 'area'))
+            if (tag['id'] == 'Coverage-Requirement'):
+                areaOrder = ['DeviceSciences', 'Circuits', 'HardwareSystems', 
+                    'SoftwareSystems', 'ProfessionalPolicy', 
+                    'SignalsAndSystems', 'UndergraduateProjects']
+                ece['Coverage-Requirement'] = (
+                    Webscrapers.updateEceAreaCourses(tag, areaOrder, 
+                    'coverage'))
+        print(ece)
+            
 
+
+
+                    
 
